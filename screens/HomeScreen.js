7c047b32179ea17543277c58232e363dccff4863
Seb <sebastien.philippe5@gmail.com>
2019-10-02 16:22:12 +0200

    Affichage des sujets sous forme de boutons sur la page QCM (fecth vers api ok)

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,25 +1,61 @@
-import React from 'react';
-import {Text,} from 'react-native';
-import {Button, Container, Header, Body, Title} from 'native-base'
+import React, {Component} from 'react';
+import {FlatList,View} from 'react-native';
+import {Button, Container, Header, Body, Title, Text, Content} from 'native-base'
 
-function findSubject() {
-  console.log('find subject');
+export  function Item({ title }) {
+    return (
+        <View>
+            <Button primary rounded>
+                <Text>{title}</Text>
+            </Button>
+        </View>
+    );
 }
 
-export default function HomeScreen() {
-  return (
-      <Container>
-        <Header>
-          <Body>
-            <Title>Home</Title>
-          </Body>
-        </Header>
-        <Button primary onPress={()=>findSubject()}>
-          <Text>Essai bouton native bas</Text>
-        </Button>
-      </Container>
-  );
+class HomeScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            subjects:[]
+        }
+    }
+
+    componentDidMount() {
+        this.findSubject();
+    }
+
+    findSubject() {
+        fetch(process.env.API_URL+'/subjects')
+            .then(response => response.json())
+            .then(subjects => this.setState({
+                subjects: subjects
+            }));
+        console.log(this.state.subjects);
+    }
+
+
+    render() {
+        return (
+            <Container>
+                <Header>
+                    <Body>
+                        <Title>QCM Titre page</Title>
+                    </Body>
+                </Header>
+                <Content padder>
+                    <FlatList
+                        data={this.state.subjects}
+                        renderItem={({item})=><Item title={item.title}/>}
+                        keyExtractor={item=>item._id}
+                    />
+                </Content>
+            </Container>
+        );
+    }
 }
+
 HomeScreen.navigationOptions = {
-  header: null,
+    title: 'QCM'
 };
+
+export default HomeScreen;
